356addf5db4237c0e45b7bab6fd140463a2643ff
jonnyv91 <vaflorjonathan@johndeere.com>
2020-11-11 15:58:26 -0600

    created seeder file for customer

---
+++ jtv-app-backend/seeders/20201111215300-demo-customer.js
@@ -0,0 +1,30 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert('Customers', [
+      {
+        customer_Fname: "Jon",
+        customer_Lname: "Vaflor",
+        customer_email: "jtv2610@gmail.com",
+        customer_phone: "222-222-2222"
+      },
+      {
+        customer_Fname: "Joe",
+        customer_Lname: "Smith",
+        customer_email: "JSmith@gmail.com",
+        customer_phone: "333-333-3333"
+      },
+      {
+        customer_Fname: "Jane",
+        customer_Lname: "Doe",
+        customer_email: "JDoe@gmail.com",
+        customer_phone: "444-444-4444"
+      },
+    ], {});
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete('Customers', null, {});
+  }
+};
