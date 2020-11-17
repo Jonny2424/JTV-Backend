826264575d5439cbe2fa8ddeb7740e5578f423d9
jonnyv91 <vaflorjonathan@johndeere.com>
2020-11-17 14:49:09 -0600

    hmmm

--- seeders/20201111215300-demo-customer.js
+++ seeders/20201111215300-demo-customer.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.bulkInsert('Customers', [
+    return queryInterface.bulkInsert('customers', [
       {
         customer_Fname: "Jon",
         customer_Lname: "Vaflor",
@@ -25,6 +25,6 @@ module.exports = {
   },
 
   down: (queryInterface, Sequelize) => {
-    return queryInterface.bulkDelete('Customers', null, {});
+    return queryInterface.bulkDelete('customers', null, {});
   }
 };
